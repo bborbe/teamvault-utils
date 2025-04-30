ebd426b6cc8eeb5fe860d70b5ba65afacee36415
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-04-30 11:55:27 +0200

    use lib service + argument

---
+++ factory/factory_suite_test.go
@@ -0,0 +1,18 @@
+package factory_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
+)
+
+//go:generate go run -mod=mod github.com/maxbrunsfeld/counterfeiter/v6 -generate
+func TestSuite(t *testing.T) {
+	time.Local = time.UTC
+	format.TruncatedDiff = false
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Suite")
+}
